5b511efc30f101ce02ff3d50d3fc9ade4e307e90
afxsta <devefosta@gmail.com>
2024-02-01 20:28:26 +0300

    use create-message at input

--- src/shared/ui/input/Input.vue
+++ src/shared/ui/input/Input.vue
@@ -1,10 +1,12 @@
 <script lang="ts" setup>
 import { computed } from 'vue'
 import { IInputProps } from '@/shared/ui/input'
+import { ErrorMessage } from '../error-message'
 
 const props = withDefaults(defineProps<IInputProps>(), {
   label: '',
-  error: '',
+  error: 'АШЫБКА',
+  disabled: true,
 })
 const emit = defineEmits<{
   /**
@@ -17,6 +19,14 @@ const value = computed({
   get: () => props.modelValue,
   set: (_value) => emit('update:modelValue', _value || ''),
 })
+
+/**
+ * * Классы поля ввода
+ */
+const inputClasses = computed(() => ({
+  error: props.error,
+  disabled: props.disabled,
+}))
 </script>
 <template>
   <div class="ui-input-wrapper f fd-col jc-c fs_1">
@@ -26,16 +36,27 @@ const value = computed({
       class="ui-input-wrapper_label"
       v-text="label"
     />
-    <input
-      id="input"
-      v-model="value"
-    />
+    <ErrorMessage>
+      <template #default>
+        <input
+          id="input"
+          :class="inputClasses"
+          v-bind="props"
+          v-model="value"
+        />
+      </template>
+      <template #error>
+        {{ error }}
+      </template>
+    </ErrorMessage>
   </div>
 </template>
 <style lang="scss" scoped>
 .ui-input-wrapper {
+  cursor: default;
+
   &:hover {
-    input {
+    input:not(.disabled) {
       background-color: var(--lightest-grey);
     }
   }
@@ -46,16 +67,28 @@ const value = computed({
   }
 
   input {
+    width: 100%;
+    height: 40px;
+    position: relative;
     outline: none;
     font-weight: 500;
     font-size: 14px;
     line-height: 24px;
     color: var(--dark-grey);
     background-color: var(--lightest-grey1);
-    transition: var(--transition-1);
-    border: none;
+    border: 1px solid transparent;
     border-radius: 4px;
     padding: 0 var(--indent-2);
+    transition: var(--transition-1);
+    cursor: text;
+
+    &.error {
+      border-color: var(--lightest-red);
+    }
+    &.disabled {
+      pointer-events: none;
+      color: var(--lightest-grey);
+    }
 
     &:focus,
     &:active {
