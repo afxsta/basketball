59ba72622876c6660e6036da40a30789d0bf6fbd
afxsta <devefosta@gmail.com>
2024-02-01 20:28:10 +0300

    create error-message component

---
+++ src/shared/ui/error-message/ErrorMessage.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="error-message-wrapper">
+    <slot />
+    <div class="error-message-wrapper_message">
+      <slot name="error" />
+    </div>
+  </div>
+</template>
+<style lang="scss" scoped>
+.error-message-wrapper {
+  position: relative;
+  font-weight: 500;
+  font-size: 12px;
+  line-height: 18px;
+
+  &_message {
+    position: absolute;
+    top: calc(100% + 2px);
+    left: 0;
+    color: var(--lightest-red);
+  }
+}
+</style>
